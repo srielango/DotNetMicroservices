ead1832f598bef045529d7a82a73dd18d21d4569
srielango <srielango@gmail.com>
2024-09-19 17:20:23 +0530

    Added PlatformService Api endpoints

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -1,5 +1,10 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,6 +31,38 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 PrepDb.PrepPopulation(app);
+
+app.MapGet("/api/Platforms", (IPlatformRepo repository, IMapper mapper) =>
+{
+    Console.WriteLine("--> Getting Platforms...");
+    var platformItem = repository.GetAllPlatforms();
+    return mapper.Map<IEnumerable<PlatformReadDto>>(platformItem);
+})
+.WithOpenApi();
+
+app.MapGet("/api/Platforms/{id}", (IPlatformRepo repository, IMapper mapper, [FromRoute] int id) =>
+{
+    Console.WriteLine("--> Getting Platform by id...");
+    var platformItem = repository.GetPlatformById(id);
+    if (platformItem != null)
+    {
+        return Results.Ok(mapper.Map<PlatformReadDto>(platformItem));
+    }
+    return Results.NotFound();
+})
+.WithName("GetPlatformById")
+.WithOpenApi();
+
+app.MapPost("/api/Platforms", (IPlatformRepo repository, IMapper mapper, PlatformCreateDto platformCreateDto) =>
+{
+    var platformModel = mapper.Map<Platform>(platformCreateDto);
+    repository.CreatePlatform(platformModel);
+    repository.SaveChanges();
+
+    var platformReadDto = mapper.Map<PlatformReadDto>(platformModel);
+    return Results.CreatedAtRoute("GetPlatformById", new { Id = platformReadDto.Id }, platformReadDto);
+})
+.WithOpenApi();
 
 // app.MapGet("/weatherforecast", () =>
 // {
